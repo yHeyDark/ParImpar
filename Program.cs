cf08da7e258baaa1ee127b23eb2eec8b72a8612f
Derik Rocha <122660784+yHeyDark@users.noreply.github.com>
2025-05-18 17:22:34 -0300

    parimpar

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿double numero;
+
+Console.WriteLine("Digite o numero: ");
+numero = Convert.ToDouble(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("Numero e par");
+}
+
+else
+{
+    Console.WriteLine("Numero e impar");
+}
